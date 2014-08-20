dda841c997a94620f81daa7044ed007c9266dfa8
Eugene Sanivsky <seugene@marvell.com>
2014-08-20 13:21:42 +0300

    linux: import changes for 3.9 from trunk

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -2,8 +2,14 @@
 
 LINUX_RELEASE?=1
 
+ifeq ($(LINUX_VERSION),3.9.11)
+  LINUX_KERNEL_MD5SUM:=edbf88eb7f7d34dbd5d3887726790755
+endif
 ifeq ($(LINUX_VERSION),3.10.49)
   LINUX_KERNEL_MD5SUM:=9774e12764e740d49c80eda77d0ef3eb
+endif
+ifeq ($(LINUX_VERSION),3.14.16)
+  LINUX_KERNEL_MD5SUM:=469c879236ed694f60b81122e91ca02e
 endif
 
 # disable the md5sum check for unknown kernel versions
